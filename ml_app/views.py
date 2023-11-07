3c9a3fab24e7a52a828799f3d9ec15c4727021ea
Manuela0214 <manuela.1701722868@ucaldas.edu.co>
2023-11-07 13:38:10 -0500

    endpoint /load

--- ml_app/views.py
+++ ml_app/views.py
@@ -11,20 +11,29 @@ def home(request):
 @csrf_exempt
 def cargar_excel(request):
     if request.method == 'POST' and request.FILES.get('archivo'):
-        excel_file = request.FILES['archivo']
-        if excel_file.name.endswith('.xls') or excel_file.name.endswith('.xlsx'):
-            # Cargar el archivo Excel y convertirlo a JSON
-            df = pd.read_excel(excel_file)
-            #json_data = json.loads(df.to_json(orient='records'))
+        archivo = request.FILES['archivo']
+        extension = archivo.name.split('.')[-1].lower()
+        
+        if extension in ['xls', 'xlsx', 'csv']:
+            if extension in ['xls', 'xlsx']:
+                # Cargar el archivo Excel
+                df = pd.read_excel(archivo)
+            elif extension == 'csv':
+                # Cargar el archivo CSV
+                df = pd.read_csv(archivo)
+
+            # Convertir el DataFrame de Pandas a un diccionario JSON
             json_data = df.to_dict(orient='records')
-            # Almacena el JSON en MongoDB
-            client = MongoClient("mongodb+srv://<user_mongo_ml>:<rhLlLiR5fcJd3Mjb>@clustermongo.6vnczd1.mongodb.net/?retryWrites=true&w=majority")
-            db = client["DB_ML_inteligentes"]
-            collection = db["coleccion"]
-            collection.insert_many(json_data)
             
-            return JsonResponse({'mensaje': 'Archivo Excel cargado y almacenado en MongoDB con éxito'})
+            # Almacena el JSON en MongoDB
+            client = MongoClient("mongodb+srv://user_mongo_ml:admin123@clustermongo.su7wh1s.mongodb.net/?retryWrites=true&w=majority")
+            db = client["BDInteligentes"]
+            collection = db["coleccionML"]
+            collection.insert_many(json_data)
+            print(json_data)
+            
+            return JsonResponse({'mensaje': f'Archivo {extension} cargado y almacenado en MongoDB con éxito'})
         else:
-            return JsonResponse({'error': 'El archivo debe ser un documento Excel'})
+            return JsonResponse({'error': 'El archivo debe ser un documento Excel o CSV'})
     else:
         return JsonResponse({'error': 'Solicitud no válida'})
