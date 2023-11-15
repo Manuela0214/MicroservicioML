55f054b648c1086eb2db6c23b80703cfbef294be
Manuela0214 <manuela.1701722868@ucaldas.edu.co>
2023-11-15 13:12:28 -0500

    implementación end point /general_univariate_graphs

--- api_ml_inteligentes/urls.py
+++ api_ml_inteligentes/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('basic statistics/<str:dataset_id>/', views.basic_statistics, name='basic_statistics'),
     path('columns-describe/<str:dataset_id>/', views.columns_describe, name='columns-describe'),
     path('imputation/<str:dataset_id>/type/<str:number_type>/', views.imputation, name='imputation'),
+    path('general-univariate-graphs/<str:dataset_id>', views.general_univariate_graphs, name='general_univariate_graphs'),
 ]
